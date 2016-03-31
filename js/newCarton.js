797d73640dcda203734dcfb03e85e2afdc276e55
carmenlm <carmenlopezmedina@yahoo.es>
2016-03-31 15:36:18 +0200

    asignar aleatorios por columna dependiendo rango //index añadido fila lentas bingo

--- js/newCarton.js
+++ js/newCarton.js
@@ -7,7 +7,9 @@
  */
 function init() {
 
+
     newCard();
+
 
 }
 
@@ -16,37 +18,122 @@ function init() {
  * Funcion para crear el carton
  */
 function newCard() {
-
-    //variable para identificar la celda
+    //variable para poner el numero de la celda
     var celda = 0;
 
+    //variable id contador para la celda
+    var id = 0;
 
+
+    //bucle para crear las 5 columnas
     for (var i = 0; i < 5; i++) {
-        var divBotones = document.createElement('div');
-        divBotones.setAttribute('class', 'btn-group');
-        divBotones.setAttribute('role', 'group');
 
 
+
+        //div1 para cada fila (bootstrap)
+        var divFila1 = document.createElement('div');
+        divFila1.setAttribute('class', 'btn-group btn-group-justified');
+        divFila1.setAttribute('role', 'group');
+
+        //bucle para crear las 5 filas
         for (var j = 0; j < 5; j++) {
-            //console.log("i: " + i + " / j: " + j);
-            //console.log("celda " + (celda));
+
+            //TODO asignar el valor
+            //celda =
+
+            //div2 para cada fila (bootstrap)
+            var divFila2 = document.createElement('div');
+            divFila2.setAttribute('class', 'btn-group');
+            divFila2.setAttribute('role', 'group');
 
 
+            //cada celda es un boton para luego ir desactivandolos
             var botonCelda = document.createElement('button');
 
-            var textoNodo = document.createTextNode(celda);
-            botonCelda.appendChild(textoNodo);
-            botonCelda.setAttribute('class', 'btn btn-default');
-            divBotones.appendChild(botonCelda);
+            //TODO poner numeros aleatorios en cada columna cada intervalo
+            //var textoNodo = document.createTextNode(celda);
+            //botonCelda.appendChild(textoNodo);
+            botonCelda.setAttribute('class', 'btn btn-primary');
+            botonCelda.setAttribute('role', 'group');
+            botonCelda.setAttribute('id', 'celda' + id);
 
-            celda++;
+            //incremento id
+            id++;
 
-            document.getElementById('panelBingo').appendChild(divBotones);
+            //añado el boton a la div2
+            divFila2.appendChild(botonCelda);
+
+            //añado el div2 al div 1 (bootstrap)
+            divFila1.appendChild(divFila2);
+
+
+            //TODO poner en el div correspondiente
+            document.getElementById('panelBingo').appendChild(divFila1);
+
 
         }
 
 
     }
+    asignarNumeroAleatorio();
+
+
+}
+/**
+ *
+ * @param columna (letra B / I / N / G / O) -- depende de la columna tiene un rango
+ * funcion para asignar un valor a la celda -- numero aleatorio
+ */
+function asignarNumeroAleatorio() {
+    //inicio de la variable numero aleatorio
+    var numero = 0;
+
+    var arrayCeldas = [];
+
+    for (var i = 0; i < 5; i++) {
+        var contador = i;
+        for (var j = 0; j < 5; j++) {
+            //variable celda
+            var celda = "celda" + contador;
+
+            console.log(contador);
+
+            console.log("i " + i + " / j " + j);
+
+
+            switch (i) {
+                case 0:
+                    numero = Math.floor(Math.random() * 15) + 1;
+                    document.getElementById(celda).innerHTML = numero;
+                    contador = contador + 5;
+                    break;
+                case 1:
+                    numero = Math.floor(Math.random() * 15) + 16;
+                    document.getElementById(celda).innerHTML = numero;
+                    contador = contador + 5;
+                    break;
+                case 2:
+                    numero = Math.floor((Math.random() * 15) + 31);
+                    document.getElementById(celda).innerHTML = numero;
+                    contador = contador + 5;
+                    break;
+                case 3:
+                    numero = Math.floor((Math.random() * 15) + 46);
+                    document.getElementById(celda).innerHTML = numero;
+                    contador = contador + 5;
+                    break;
+                case 4:
+                    numero = Math.floor((Math.random() * 15) + 61);
+                    document.getElementById(celda).innerHTML = numero;
+                    contador = contador + 5;
+                    break;
+            }
+
+        }
+
+
+    }
+
 
 }
 
